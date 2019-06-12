95962f44555257bdbbc49fb583048aa5ebfa6c86
sanchitb23 <sanbht23@gmail.com>
2019-06-13 01:35:12 +0530

    Implemented Observables. Changed EventEmitters with Subjects(rxjs) for data exchange between components

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,27 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private activeSubscription: Subscription;
 
   constructor(private shopListService: ShoppingListService) {
   }
 
   ngOnInit() {
     this.ingredients = this.shopListService.getIngredients();
-    this.shopListService.ingredientsChanged
+    this.activeSubscription = this.shopListService.ingredientsChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
         }
       );
 
+  }
+
+  ngOnDestroy(): void {
+    this.activeSubscription.unsubscribe();
   }
 
 }
