99280be96b546ef3bfbcd71c0fa3cc0f2fdd88dc
sanchitb23 <sanbht23@gmail.com>
2019-06-26 15:57:16 +0530

    Divided AppModule to Multiple Modules (2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,37 +3,31 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {RecipeService} from './recipes/recipe.service';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {AuthComponent} from './auth/auth.component';
-import {AuthInterceptorService} from './auth/auth-interceptor.service';
+import {HttpClientModule} from '@angular/common/http';
 import {RecipesModule} from './recipes/recipes.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core.module';
+import {AuthModule} from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     RecipesModule,
-    ReactiveFormsModule,
     HttpClientModule,
-
     AppRoutingModule,
+
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule,
+    AuthModule
   ],
-  providers: [ShoppingListService, RecipeService, {
-    provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true
-  }],
+  // providers: [CoreModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {
